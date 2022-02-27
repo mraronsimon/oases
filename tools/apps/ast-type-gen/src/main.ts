82b4c0104127fdd1ae24a339b89dfb69dd37a296
Aron Simon <aron.simon.developer@gmail.com>
2022-02-27 15:47:24 +0100

    feat: create executor main

--- tools/apps/ast-type-gen/src/main.ts
+++ tools/apps/ast-type-gen/src/main.ts
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+import { ExecutorContext } from '@nrwl/devkit';
+
+export interface AstTypeGeneratorOptions {
+  descriptorFilePath: string;
+}
+
+export default async function astTypeGenerator(
+  options: AstTypeGeneratorOptions,
+  context: ExecutorContext
+) {
+  // Validate context
+  if (!context.projectName) {
+    console.error('Project must be included!');
+    return { success: false };
+  }
+
+  const descriptorFilePath = `${context.root}/${options.descriptorFilePath}`;
+
+  const projectPath = `${context.root}/${
+    context.workspace.projects[context.projectName].root
+  }`;
+
+  return { success: true };
+}
